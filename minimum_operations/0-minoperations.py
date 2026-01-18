bfc3d1eb5f0047a788e092a646e57e091c8560af
julesineza <j.ineza@alustudent.com>
2026-01-18 16:53:42 +0200

    solution

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+"""
+
+def minOperations(n):
+    """
+    a method that calculates the fewest number of operations needed to result in exactly n H characters in the file.
+
+    Returns :
+        an interger
+    if n is impossible, returns 0
+    """
+    if n < 2:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
+
+
+
+
